1ddbbf7cb7c7d52c201d352b2618dab07b86d439
SabbirAbdullah <sabbir.abdullah.dev@gmail.com>
2025-05-27 11:31:39 +0600

    first commit

---
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.kotlinbasics
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kotlinbasics.ui.theme.KotlinBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinBasicsTheme {
+                GreetingScreen(name = "I am Sabbir", age = 24)
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingScreen(name: String, age: Int) {
+    val message = greetUser(name, age)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center,
+
+    ) {
+        Box {
+
+            Text(
+                text = message,
+                color = Color.Black,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp).background(color = Color.Green),
+
+            )
+        }
+    }
+}
+
+fun greetUser(name: String, age: Int): String {
+    val message = "Hello $name! I am  $age years old."
+    Log.d("KotlinDay1", message)
+    return message
+}
